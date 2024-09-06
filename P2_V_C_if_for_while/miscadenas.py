8d368d374177987b2e18d3e345dd4126603f328b
ReyesRamirez <a.22308051281289@cbtis128.edu.mx>
2024-09-06 09:35:28 -0600

    agregue 2 archivos

---
+++ P2_V_C_if_for_while/miscadenas.py
@@ -0,0 +1,34 @@
+'''
+Reyes Ramirez
+Trabajo con cadenas
+'''
+
+nombre="Reyes"
+print(nombre[0])
+print(nombre)
+
+MisNovias=["E","l","i"]
+for chicas in MisNovias:
+    print(chicas)
+
+print("otro ejemplo")
+b = "Reyes Ramirez"
+print(b[2:5])
+
+print(" a mayusculas")
+a = "Reyes Ramirez"
+print(a.upper())
+
+print("Concatenar")
+a = "Maria"
+b = "Perez"
+c = a + "_" + b
+print(c)
+
+
+
+
+
+
+        
+        
---
+++ P2_V_C_if_for_while/mivariables.py
@@ -0,0 +1,19 @@
+import math
+edad=65  #65 se guarda en edad
+
+nombre="Daniela Lopez"
+print("tu edad es", edad)
+print("te llamas",nombre)
+print("-- tipo de variables ---")
+x = str(7)    # x will be '3'
+y = int(3.1416)    # y will be 3
+z = float(7)
+print(x)
+print(y)
+print(z)
+print("-- funciones matematicas ----")
+x = math.sqrt(64)
+print(x)
+
+
+
